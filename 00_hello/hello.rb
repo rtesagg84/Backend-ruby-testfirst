2fc04f3c9569970f9ca44cca75af495522071583
rtesagg84 <rtesagg84@gmail.com>
2021-11-12 23:16:28 -0500

    part of the errors soved

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,8 @@
 #write your code here
+def hello
+"Hello!"
+end
+
+def greet(aster)
+"Hello, #{aster}!"
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
-#write your code here
+
+def add(a,b)
+    a + b
+end
+def subtract(a,b)
+    a - b
+end
+def sum array
+    count =0
+  array.each do|item|
+    count += item 
+  end
+count
+end
+def multiplies(a,b)
+  a * b
+end
+
+
+def multiply args
+	mult = 1
+	if args.length <= 1
+		puts "You need to input at least two integers"
+	else
+		args.each do |num| 
+			mult = num * mult
+		end
+	end
+	mult
+end
